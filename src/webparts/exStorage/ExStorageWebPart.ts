82081296cef2b0d403538da5a2e259bc17409f14
mikezimm <49648086+mikezimm@users.noreply.github.com>
2021-09-17 12:39:27 -0400

    built first package using this version

--- src/webparts/exStorage/ExStorageWebPart.ts
+++ src/webparts/exStorage/ExStorageWebPart.ts
@@ -3,7 +3,8 @@ import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
+  PropertyPaneTextField,
+  PropertyPaneToggle,
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 
@@ -26,6 +27,8 @@ import { IExStorageProps } from './components/IExStorageProps';
 // import { WebPartInfoGroup, JSON_Edit_Link } from '@mikezimm/npmfunctions/dist/Services/PropPane/zReusablePropPane';
 import * as links from '@mikezimm/npmfunctions/dist/HelpInfo/Links/LinksRepos';
 
+require('../../services/GrayPropPaneAccordions.css');
+
 export interface IExStorageWebPartProps {
   description: string;
 
@@ -39,6 +42,9 @@ export interface IExStorageWebPartProps {
 
   parentWeb: string;
   listTitle: string;
+  showListDropdown: boolean;
+  showSystemLists: boolean;
+  excludeListTitles: string;
 
   //General settings for FPS Options group
   searchShow: boolean;
@@ -52,18 +58,23 @@ export interface IExStorageWebPartProps {
 
 export default class ExStorageWebPart extends BaseClientSideWebPart<IExStorageWebPartProps> {
 
+  private currentSite: string = window.location.href;
   private minQuickLaunch: boolean = false;
   private fpsPageDone: boolean = false;
   private fpsPageArray: any[] = null;
   private currentUser: IUser = null;
+  private urlVars : any;
+  private allowOtherSites: boolean = false;
 
   public onInit():Promise<void> {
     return super.onInit().then(_ => {
 
       // other init code may be present
 
-      let mess = 'onInit - ONINIT: ' + new Date().toLocaleTimeString();
+      this.urlVars = this.getUrlVars();
+      console.log('urlVars:' , this.urlVars );
 
+      let mess = 'onInit - ONINIT: ' + new Date().toLocaleTimeString();
       console.log(mess);
 
       //https://stackoverflow.com/questions/52010321/sharepoint-online-full-width-page
@@ -109,17 +120,32 @@ export default class ExStorageWebPart extends BaseClientSideWebPart<IExStorageWe
   public render(): void {
 
     //For FPS Options
+
     this.setThisPageFormatting( this.properties.fpsPageStyle );
     this.setQuickLaunch( this.properties.quickLaunchHide );
 
     //Be sure to always pass down an actual URL if the webpart prop is empty at this point.
     //If it's undefined, null or '', get current page context value
     // let parentWeb = this.properties.parentWeb && this.properties.parentWeb != '' ? this.properties.parentWeb : this.context.pageContext.web.absoluteUrl;
-    let parentWeb = 'https://autoliv.sharepoint.com/sites/MSLV5Generaltasks/';
-    let tenant = this.context.pageContext.web.absoluteUrl.replace(this.context.pageContext.web.serverRelativeUrl,"");
+    let parentWeb = this.properties.parentWeb;
+    if ( !parentWeb || parentWeb.length === 0 ) {
+      // debugger;
+      if ( this.currentSite.toLowerCase().indexOf('webpartdev') > -1 ) {
+        this.allowOtherSites = true;
+        parentWeb = 'https://autoliv.sharepoint.com/sites/MSLV5Generaltasks/';
+      } else {
+        parentWeb = this.context.pageContext.web.absoluteUrl;
+        this.properties.parentWeb = this.context.pageContext.web.absoluteUrl;
+      }
+    }
 
-    let urlVars : any = this.getUrlVars();
-    console.log('urlVars:' , urlVars );
+    if ( this.urlVars.allowOtherSites === 'true' ) {
+      this.allowOtherSites = true;
+    }
+    
+    // let tenant = this.context.pageContext.web.absoluteUrl.replace(this.context.pageContext.web.serverRelativeUrl,"");
+    let tenant = window.location.origin;
+
 
     const element: React.ReactElement<IExStorageProps> = React.createElement(
       ExStorage,
@@ -128,7 +154,7 @@ export default class ExStorageWebPart extends BaseClientSideWebPart<IExStorageWe
         pageContext: this.context.pageContext,
         wpContext: this.context,
         tenant: tenant,
-        urlVars: urlVars,
+        urlVars: this.urlVars,
     
         //Size courtesy of https://www.netwoven.com/2018/11/13/resizing-of-spfx-react-web-parts-in-different-scenarios/
         WebpartElement: this.domElement,
@@ -143,9 +169,9 @@ export default class ExStorageWebPart extends BaseClientSideWebPart<IExStorageWe
         WebpartWidth:  this.domElement.getBoundingClientRect().width - 50 ,
 
         parentWeb: parentWeb,
-        listTitle: 'Documents',
+        listTitle: this.properties.listTitle,
     
-        allowOtherSites: true, //default is local only.  Set to false to allow provisioning parts on other sites.
+        allowOtherSites: this.allowOtherSites, //default is local only.  Set to false to allow provisioning parts on other sites.
         pickedWeb : null,
         theSite: null,
 
@@ -174,15 +200,39 @@ export default class ExStorageWebPart extends BaseClientSideWebPart<IExStorageWe
           header: {
             description: strings.PropertyPaneDescription
           },
+          displayGroupsAsAccordion: true,
           groups: [
             // WebPartInfoGroup( links.gitRepoEasyContnets, 'For analyzing extreme document libraries' ),
             // FPSOptionsGroup( false, true, true, true ), // this group,
             {
               groupName: strings.BasicGroupName,
+              isCollapsed: false ,
               groupFields: [
                 PropertyPaneTextField('parentWeb', {
-                  label: 'Site URL'
-                })
+                  label: 'Site URL',
+                  description: 'Will load current site by default',
+                  disabled: this.allowOtherSites !== true ? true : false,
+                }),
+
+                PropertyPaneTextField('listTitle', {
+                  label: 'Library Title'
+                }),
+
+                PropertyPaneToggle('showListDropdown', {
+                  label: 'Show Lists Dropdown in webpart'
+                }),
+
+                PropertyPaneToggle('showSystemLists', {
+                  label: 'Show System Lists',
+                  disabled: this.properties.showListDropdown === true ? false : true,
+                }),
+
+                PropertyPaneTextField('excludeListTitles', {
+                  label: 'Exclude these from dropdown',
+                  disabled: this.properties.showListDropdown === true ? false : true,
+                  description: 'semi-colon separated words'
+                }),
+
               ]
             }
           ]
